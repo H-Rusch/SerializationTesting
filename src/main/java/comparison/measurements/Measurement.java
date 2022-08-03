74b7311b2880d00960960026c3fcb3a9ce90fb17
H-Rusch <kirdneh98mail@gmail.com>
2022-08-03 15:20:54 +0200

    compact output

--- src/main/java/comparison/measurements/Measurement.java
+++ src/main/java/comparison/measurements/Measurement.java
@@ -12,12 +12,7 @@ public abstract class Measurement {
     // divisor to get microseconds
     private final double divisor = 1000000;
 
-    // constants for objects to serialize
-    // TODO remove
-    private final String NAME = "Hendrik";
-    private final String EMAIL = "hallo@email.com";
-    private final int ID = 1;
-    private final String NUMBER = "123-4567";
+    private boolean pretty = false;
 
     protected Measurement(String measurementName, String filename, String fileExtension) {
         this.measurementName = measurementName;
@@ -25,20 +20,8 @@ public abstract class Measurement {
         this.fileExtension = fileExtension;
     }
 
-    public String getNAME() {
-        return NAME;
-    }
-
-    public String getEMAIL() {
-        return EMAIL;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public String getNUMBER() {
-        return NUMBER;
+    public void setPretty(boolean pretty) {
+        this.pretty = pretty;
     }
 
     public final void measure(int number) throws IOException {
@@ -69,8 +52,12 @@ public abstract class Measurement {
         // save to file
         saveToFile(bytes, number);
 
-        // print info
-        printInfo(number, measurementSerialize, measurementDeserialize);
+        if (pretty) {
+            // print info
+            printInfo(number, measurementSerialize, measurementDeserialize);
+        } else {
+            printInfoCompact(number, measurementSerialize, measurementDeserialize);
+        }
     }
 
     public final Object buildObject(int number) {
@@ -129,6 +116,13 @@ public abstract class Measurement {
                 " ns = " + measurementDeserialize / divisor + " ms");
     }
 
+    private void printInfoCompact(int number, long measurementSerialize, long measurementDeserialize) {
+        String string = String.format("%-30s", measurementName + " case " + number);
+        string = String.format("%-40s", string + "\t" + measurementSerialize);
+        string = String.format("%-50s", string + "\t" + measurementDeserialize);
+        System.out.println(string);
+    }
+
     private void saveToFile(byte[] bytes, int number) {
         String fullFilename = "measurement_data" + File.separator + "data" + number + File.separator +
                 filename + "_data" + number + "." + fileExtension;
@@ -141,26 +135,38 @@ public abstract class Measurement {
 
     // Specific methods for serializing the different test cases
     public abstract Object buildObject1();
+
     public abstract byte[] serializeObject1(Object object) throws IOException;
+
     public abstract Object deserializeObject1(byte[] bytes) throws IOException;
 
     public abstract Object buildObject2();
+
     public abstract byte[] serializeObject2(Object object) throws IOException;
+
     public abstract Object deserializeObject2(byte[] bytes) throws IOException;
 
     public abstract Object buildObject3();
+
     public abstract byte[] serializeObject3(Object object) throws IOException;
+
     public abstract Object deserializeObject3(byte[] bytes) throws IOException;
 
     public abstract Object buildObject4();
+
     public abstract byte[] serializeObject4(Object object) throws IOException;
+
     public abstract Object deserializeObject4(byte[] bytes) throws IOException;
 
     public abstract Object buildObject5();
+
     public abstract byte[] serializeObject5(Object object) throws IOException;
+
     public abstract Object deserializeObject5(byte[] bytes) throws IOException;
 
     public abstract Object buildObject6();
+
     public abstract byte[] serializeObject6(Object object) throws IOException;
+
     public abstract Object deserializeObject6(byte[] bytes) throws IOException;
 }
